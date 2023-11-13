f184fa9fa0fa98a06eee54a7cbc7e0b7c4681cb7
Sanjit Raman <ssr321@ic.ac.uk>
2023-11-13 09:46:09 +0000

    Solved 100 Same Tree Recursively.

---
+++ 100_same_tree/100_same_tree.cpp
@@ -0,0 +1,34 @@
+#include "../tools/leetcode_binary_tree_helper.h"
+
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		// base case: 
+		if (p == nullptr && q == nullptr) return true;
+		// if only one of the nodes is null, they are not identical:
+		if ((p == nullptr) || (q == nullptr)) return false;
+		
+
+		// inductive step:
+		// check if values are equal and recursively check left and right subtrees
+		if (p->val == q->val)
+			return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+
+		return false;
+	}
+};
+
+int main() {
+	BinaryTreeHelper bth;
+	std::queue<std::string> q1 = bth.queue_binary_tree();
+	std::queue<std::string> q2 = bth.queue_binary_tree();
+
+	TreeNode* p = bth.construct_binary_tree(q1);
+	TreeNode* q = bth.construct_binary_tree(q2);
+
+	Solution s;
+	bool result = s.isSameTree(p, q);
+
+	std::cout << result << std::endl;
+	
+}
