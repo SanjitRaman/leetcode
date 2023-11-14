d13416fbbf4091ce63f8eec561eb4aedf739f17d
Sanjit Raman <ssr321@ic.ac.uk>
2023-11-14 09:17:09 +0000

    Used a bfs to construct binary tree from level order traversal list. This fixes a ladder construction.

--- tools/leetcode_binary_tree_helper.h
+++ tools/leetcode_binary_tree_helper.h
@@ -3,6 +3,7 @@
 #include <queue>
 #include <string>
 #include <regex>
+#include <climits>
 
 struct TreeNode {
     int val;
@@ -16,54 +17,74 @@ struct TreeNode {
 
 class BinaryTreeHelper {
 private:
-    TreeNode* recurse_level_order(TreeNode* root, std::queue<int>& q) {
-        if (!q.empty()) {
-            if (q.front() != INT_MIN) root->left = new TreeNode(q.front());
-            q.pop();
-        }
-        if (!q.empty()) {
-            if (q.front() != INT_MIN) root->right = new TreeNode(q.front());
-            q.pop();
-        }
+    TreeNode* recurse_level_order(std::queue<int>& q) {
+        if (q.empty()) return nullptr;
 
-        if (root->left != nullptr) {
-            root->left = recurse_level_order(root->left, q);
-        }
-        if (root->right != nullptr) {
-            root->right = recurse_level_order(root->right, q);
-        }
+        int front = q.front();
+        q.pop();
 
-        return root;
+        if (front == INT_MIN) return nullptr;
+
+        TreeNode* node = new TreeNode(front);
+        node->left = recurse_level_order(q);
+        node->right = recurse_level_order(q);
+
+        return node;
     }
+
 public:
-    std::queue<int> queue_binary_tree() {
+    std::queue<std::string> queue_binary_tree() {
         std::string s, value;
-        std::queue<int> q;
+        std::queue<std::string> q;
         std::getline(std::cin, s);
 
         std::istringstream ss{ std::regex_replace(s, std::regex{R"(\[|\]|,)"}, " ") };
 
         while (ss >> value) {
-            if (value == "null") {
-                q.push(INT_MIN);
-            }
-            else {
-                q.push(stoi(value));
-            }
+            q.push(value);
 
         }
 
         return q;
     }
 
-    TreeNode* construct_binary_tree(std::queue<int>& q) {
-        TreeNode* root = new TreeNode(q.front());
+
+    TreeNode* construct_binary_tree(std::queue<std::string>& q) {
+        if (q.empty()) return nullptr;
+
+        TreeNode* root = new TreeNode(stoi(q.front()));
         q.pop();
 
-        root = recurse_level_order(root, q);
+
+        // start a bfs
+        std::queue<TreeNode*> nodes;        
+        nodes.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = nodes.front();
+            nodes.pop();
+
+            // process the left node.
+            if (!q.empty()) {
+                std::string leftVal = q.front();
+                q.pop();
+                if (leftVal != "null") {
+                    node->left = new TreeNode(stoi(leftVal));
+                    nodes.push(node->left);
+                }
+            }
+
+            // process the right node.
+            if (!q.empty()) {
+                std::string rightVal = q.front();
+                q.pop();
+                if (rightVal != "null") {
+                    node->right = new TreeNode(stoi(rightVal));
+                    nodes.push(node->right);
+                }
+            }
+        }
 
         return root;
     }
-
-
 };
