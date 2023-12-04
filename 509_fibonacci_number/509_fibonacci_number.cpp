af1ef49ded34993f243277ecd0e1e5da26ac7bb7
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-04 21:08:55 +0000

    improved efficiency of 509_fibonacci_number.cpp

--- 509_fibonacci_number/509_fibonacci_number.cpp
+++ 509_fibonacci_number/509_fibonacci_number.cpp
@@ -51,16 +51,18 @@ public:
     // only store these values.
     int fib(int n) {
         if (n==0) return 0;
-        if (n==1) return 1;
-        int a = 0, b = 1;
-        int c;
-        for (int i = 2; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
+        else if (n==1) return 1;
+        else {
+            int a = 0, b = 1;
+            int c;
+            for (int i = 2; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            // at the end, c = F(n)
+            return c;
         }
-        // at the end, c = F(n)
-        return c;
     }
 };
 
