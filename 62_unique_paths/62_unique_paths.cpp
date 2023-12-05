bc63b26b28f391cee089196d44d9568d77124b38
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-05 19:26:05 +0000

    Solved 62. Unique Paths

---
+++ 62_unique_paths/62_unique_paths.cpp
@@ -0,0 +1,32 @@
+#include "../tools/leetcode_helper.h"
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        std::vector<int> row_below(n,1);
+        
+        // for each row:
+        for(int i = 0; i < m-1; i++) {
+            std::vector<int> curr_row(n, 1);
+            // work from right to left
+            for(int j = n-2; j >=0; j--) {
+                curr_row[j] = curr_row[j+1] + row_below[j];
+            }
+            // set the row_below as the curr_row for the next iter.
+            row_below = curr_row;
+        }
+        return row_below[0];
+    }
+};
+
+int main() {
+  Solution s;
+  LeetcodeInput li;
+
+  int m, n;
+  while(li.read_integer(&m)) {
+    if(li.read_integer(&n))
+        li.print_integer(s.uniquePaths(m, n));
+  }
+  return 0;
+}
