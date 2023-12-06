b146e62da82351620e3f97b192169b94e20b673e
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-06 22:24:19 +0000

    "Optimized: Improved performance of numSquares function in 279_perfect_squares.cpp by switching to bottom-up dynamic programming approach"

--- 279_perfect_squares/279_perfect_squares.cpp
+++ 279_perfect_squares/279_perfect_squares.cpp
@@ -5,63 +5,84 @@
 class Solution {
 public:
 
-    int solve(int x, std::unordered_map<int, int>& dp, std::vector<int>& c) {
-        if (dp.find(x) != dp.end()) { // found in dp.
-            return dp[x];
-        }
-        else {
-            // compute
-            // iterate over vector c
-            int best_sol = -1;
-            int solution;
-            for(int k = 0; k < c.size(); k++) {
-                if(x-c[k] >= 0) {
-                    solution = solve(x-c[k], dp, c);
-                    if (best_sol == -1 || solution < best_sol)
-                        best_sol = solution;
-                }
-            }
-            best_sol++; // 1 more for the count of the coin we just added.
-            dp.insert(std::make_pair(x,best_sol));  
-            return best_sol; 
-        }
-    }
+    // int solve(int x, std::unordered_map<int, int>& dp, std::vector<int>& c) {
+    //     if (dp.find(x) != dp.end()) { // found in dp.
+    //         return dp[x];
+    //     }
+    //     else {
+    //         // compute
+    //         // iterate over vector c
+    //         int best_sol = -1;
+    //         int solution;
+    //         for(int k = 0; k < c.size(); k++) {
+    //             if(x-c[k] >= 0) {
+    //                 solution = solve(x-c[k], dp, c);
+    //                 if (best_sol == -1 || solution < best_sol)
+    //                     best_sol = solution;
+    //             }
+    //         }
+    //         best_sol++; // 1 more for the count of the coin we just added.
+    //         dp.insert(std::make_pair(x,best_sol));  
+    //         return best_sol; 
+    //     }
+    // }
+
+    // int numSquares(int n) {
+    //     // this is like the coin change problem
+    //     // take the highest denomination you can first.
+        
+    //     // so try a 
+    //     // greedy algo:
+    //     // always start with the largest coin you can
+    //     // assume the coins are sorted in decreasing size:
+    //     // for (k=1 to n) 
+    //     //      m_k = floor(x/ck) # number of coin ck
+    //     //      x -= mk*ck
+    //     // only works for canonical systems.
+    //     // by counterexample:
+    //     // 12: 4 4 4 is a better solution that 9 1 1 1
+    //     // Hence it is not canonical and a greedy algorithm will not work.
+
+
+    //     // instead, use a
+    //     // dynamic programming approach
+    //     // M(x) = smallest number of coins to reach target x
+    //     // M(x) = min_k=1...n { 1 + M(x-c_k) } c_k is the value of the coin we just added.
+    //     // complexity is O(n) * O(x) = O(nx)
+        
+    //     // create a vector storing squares for the non-canonical system upto sqrt(n)
+    //     // allowed to use sqrt() as the overall solution will be faster than O(n).
+    //     std::vector<int> c;
+    //     for(int i  = 1; i <= std::sqrt(n); i++) 
+    //         c.push_back(i*i);
+        
+    //     std::unordered_map<int, int> dp;
+    //     dp.insert(std::make_pair(1,1));
+
+
+    //     return solve(n, dp, c);
+
+    // }
+
+    // the above code is *Really* slow!
+    // due to the recursion inside the for loop.
+
+    // instead calculate dp[i] as
+    // for each i from 1...n
+    //      dp[i] = min(dp[i], dp[i-j*i])
+    // for all j that are square numbers less than i
+    // i.e. update the minimum number of perfect square numbers
+    // that sum to i if removing a larger square number improves on the result.
 
     int numSquares(int n) {
-        // this is like the coin change problem
-        // take the highest denomination you can first.
-        
-        // so try a 
-        // greedy algo:
-        // always start with the largest coin you can
-        // assume the coins are sorted in decreasing size:
-        // for (k=1 to n) 
-        //      m_k = floor(x/ck) # number of coin ck
-        //      x -= mk*ck
-        // only works for canonical systems.
-        // by counterexample:
-        // 12: 4 4 4 is a better solution that 9 1 1 1
-        // Hence it is not canonical and a greedy algorithm will not work.
-
-
-        // instead, use a
-        // dynamic programming approach
-        // M(x) = smallest number of coins to reach target x
-        // M(x) = min_k=1...n { 1 + M(x-c_k) } c_k is the value of the coin we just added.
-        // complexity is O(n) * O(x) = O(nx)
-        
-        // create a vector storing squares for the non-canonical system upto sqrt(n)
-        // allowed to use sqrt() as the overall solution will be faster than O(n).
-        std::vector<int> c;
-        for(int i  = 1; i <= std::sqrt(n); i++) 
-            c.push_back(i*i);
-        
-        std::unordered_map<int, int> dp;
-        dp.insert(std::make_pair(1,1));
-
-
-        return solve(n, dp, c);
-
+        std::vector<int> dp(n + 1, n); // at most n coins.
+        dp[0] = 0;
+        for(int i = 1; i <= n; ++i) {
+            for(int j = 1; j * j <= i; ++j) {
+                dp[i] = std::min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
     }
 };
 
