318d5d961bb8dbfabe9b201efd35e23a81b1873c
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-17 17:16:12 +0000

    add solution and main

--- 63_unique_paths_II/63_unique_paths_II.cpp
+++ 63_unique_paths_II/63_unique_paths_II.cpp
@@ -1,1 +1,19 @@
 #include "../tools/leetcode_helper.h"
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(std::vector<std::vector<int>>& obstacleGrid) {
+        
+    }
+};
+
+int main() {
+  Solution s;
+  LeetcodeInput li;
+
+  std::vector<std::vector<int>> obstacleGrid;
+  while(li.read_leetcode_list(obstacleGrid)) {
+    li.print_integer(s.uniquePathsWithObstacles(obstacleGrid));
+  }
+  return 0;
+}
