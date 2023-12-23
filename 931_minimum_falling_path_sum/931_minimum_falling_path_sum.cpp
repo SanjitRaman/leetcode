9ebe6e5d6b24510a1a6c2dbaf5de89f8f6ee9f1f
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-23 01:21:43 +0000

    cleaned up code

--- 931_minimum_falling_path_sum/931_minimum_falling_path_sum.cpp
+++ 931_minimum_falling_path_sum/931_minimum_falling_path_sum.cpp
@@ -1,35 +1,34 @@
 #include "../tools/leetcode_helper.h"
-#include <limits>
 
 class Solution
 {
 public:
-    int min3(int a, int b, int c)
-    {
-        return std::min(a, std::min(b, c));
-    }
-
     int minFallingPathSum(std::vector<std::vector<int>> &matrix)
     {
         int n = matrix.size();
 
+        // Initialize dp array with a large value
         std::vector<int> dp(n + 2, 10001);
+
+        // Initialize the last row of dp array with the values from the last row of the matrix
         for (int i = 0; i < n; i++)
             dp[i + 1] = matrix[n - 1][i];
 
         int prevComputedValue, computedValue;
         for (int r = n - 2; r >= 0; r--)
         {
-            int prevComputedValue = matrix[r][0] + min3(dp[0], dp[1], dp[2]);
+            // Compute the minimum falling path sum for each element in the current row
+            prevComputedValue = matrix[r][0] + std::min({dp[0], dp[1], dp[2]});
             for (int c = 1; c < n; c++)
             {
-                computedValue = matrix[r][c] + min3(dp[c], dp[c + 1], dp[c + 2]);
+                computedValue = matrix[r][c] + std::min({dp[c], dp[c + 1], dp[c + 2]});
                 dp[c] = prevComputedValue;
                 prevComputedValue = computedValue;
             }
             dp[n] = computedValue;
         }
 
+        // Find the minimum falling path sum from the first row of the dp array
         return *std::min_element(dp.begin() + 1, dp.end() - 1);
     }
 };
